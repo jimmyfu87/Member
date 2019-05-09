f540416699dd0cd47ae4d235234ae1cfd623edf0
jimmyfu87 <jimmyfu87@gmail.com>
2019-05-09 21:43:48 +0800

    Add-J-可以登入

--- app/src/main/java/com/example/member/Log.java
+++ app/src/main/java/com/example/member/Log.java
@@ -6,6 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Log extends AppCompatActivity {
 
@@ -13,10 +28,48 @@ public class Log extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log);
-        final EditText username=(EditText) findViewById(R.id.id);
-        final EditText password=(EditText) findViewById(R.id.password);
+        final EditText etusername=(EditText) findViewById(R.id.id);
+        final EditText etpassword=(EditText) findViewById(R.id.password);
         final Button blogin=(Button)findViewById(R.id.Log);
         final Button reg=(Button)findViewById(R.id.Reg);
+
+        blogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = etusername.getText().toString();
+                final String password = etpassword.getText().toString();
+
+                // Response received from the server
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if (success) {
+                                Intent intent = new Intent(Log.this, success_log.class);
+                                Log.this.startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Log.this);
+                                builder.setMessage("Login Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                LoginRequest loginRequest = new LoginRequest(username, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(Log.this);
+                queue.add(loginRequest);
+            }
+        });
+
 
     reg.setOnClickListener(new View.OnClickListener() {
         @Override
@@ -25,6 +78,7 @@ public class Log extends AppCompatActivity {
             Log.this.startActivity(registerIntent);
         }
     });
+
 
 
     }
---
+++ app/src/main/java/com/example/member/success_log.java
@@ -0,0 +1,12 @@
+package com.example.member;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class success_log extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_success);
+
+    }
+}
